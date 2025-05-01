db93ecfc5bb0346e47a89dd4ef15c80ea3195546
CodingDeni-boop <dbugatti@ethz.ch>
2025-05-01 18:42:39 +0200

    good

--- project.py
+++ project.py
@@ -8,6 +8,7 @@ features=pd.read_csv("../DRIAMS-EC/driams_Escherichia_coli_Ceftriaxone_features.
 labels=pd.read_csv("../DRIAMS-EC/driams_Escherichia_coli_Ceftriaxone_labels.csv")
 data=features.merge(labels)
 
+#QOGNQEG
 
 
 
