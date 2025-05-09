7dc2e6f1c20907c16f162960ba6f99d8105af7ae
CodingDeni-boop <dbugatti@ethz.ch>
2025-05-09 21:15:32 +0200

    K-FOLD DONE

--- project/code/project.py
+++ project/code/project.py
@@ -23,13 +23,23 @@ def checkna(data):
     print(f"dataset has {np.sum(np.sum(datana,axis=1))} NA")
 
 ## I DONT LIKE NAMES 0798f178-95d5-4e92-ad63-831cf50605b5_MALDI2, I WANT MALDI2, THAT'S IT, THEIR NAME WILL BE THEIR INDEX. ACTUALLY, M1=0, M2=1
-data = data.rename(columns={"Unnamed: 0" : "type"})
-M1 = np.sum(data.loc[:,"type"].str.contains("MALDI1"))
-M2 = np.sum(data.loc[:,"type"].str.contains("MALDI2"))
-data.loc[data.loc[:,"type"].str.contains("MALDI1"), "type"] = "M1"
-data.loc[data.loc[:,"type"].str.contains("MALDI2"), "type"] = "M2"
+def maldiRename(data):
+    data = data.rename(columns={"Unnamed: 0" : "type"})
+    M1 = np.sum(data.loc[:,"type"].str.contains("MALDI1"))
+    M2 = np.sum(data.loc[:,"type"].str.contains("MALDI2"))
+    data.loc[data.loc[:,"type"].str.contains("MALDI1"), "type"] = "M1"
+    data.loc[data.loc[:,"type"].str.contains("MALDI2"), "type"] = "M2"
+    print(f"There are: M1 {M1}, M2 {M2}")
+    return data
 
-print(f"There are: M1 {M1}, M2 {M2}")
+def createDummies(data):
+    dummies=pd.get_dummies(data["type"],drop_first=True)
+    dummies.reset_index(inplace=True)
+    data.reset_index(inplace=True)
+    data=data.merge(dummies,on="index",how="left")
+    data=data.drop(["type","index"],axis=1)
+    data["M2"]=data["M2"].astype(float)
+    return data
 
 ##  MAYBE TYPE EXPLAINS THE LABEL SOME WAY, actually no... maybe M2 has slightly more probability of label 1, but not much is seen.
 ##  PLOT M1 - M2 vs label
@@ -38,7 +48,6 @@ def m1_m2_plotter(data):
     fig = plt.figure(figsize=(6,6)) 
     sns.countplot(data=data,x="label",hue="type")
     plt.savefig("../output/M1-M2 vs label")
-    
 
 ###CHECK FOR DOUBLES
 def drop_duplicates(data):
@@ -49,32 +58,41 @@ def drop_duplicates(data):
         print("dataset had duplicates and they have been dropped")
     return data
 
-
-##CREATE DUMMIES FROM M1 - M2 STRINGS TO 0.0 - 1.0 DUMMIES
-dummies=pd.get_dummies(data["type"],drop_first=True)
-dummies.reset_index(inplace=True)
-data.reset_index(inplace=True)
-data=data.merge(dummies,on="index",how="left")
-data=data.drop(["type","index"],axis=1)
-data["M2"]=data["M2"].astype(float)
-
 ###HEATMAP PLOTTER
-"""
-corr=data.corr()
-plt.figure(figsize=(300,300))
-sns.heatmap(corr)
-plt.savefig("../output/heatmap")
-print("plotted")
-"""
+def heatmapPlotter(data):
+    corr=data.corr()
+    plt.figure(figsize=(300,300))
+    sns.heatmap(corr)
+    plt.savefig("../output/heatmap")
+    print("plotted")
 
-###WORK IN PROGRESS
+def splitData(data):
+    tune, test = skm.train_test_split(data, test_size=0.15)
+    tune.reset_index(inplace=True)
+    tune.drop(axis=1, inplace=True,columns="index")
+    test.reset_index(inplace=True)
+    test.drop(axis=1, inplace=True,columns="index")
+    print("data splitted")
+    print(f"tune: {tune.shape}\n test: {test.shape}")
+    return tune, test
 
+### K - FOLD:    SPLITTING INTO 5, ACCESSIBLE THROUGH LIST, SHUFFLE TRUE
+def kfolderino(data):
+    traininglist = []
+    validatelist = []
+    kfolder = skm.KFold(n_splits = 5,shuffle=True,random_state=2025)
+    for train, validate in kfolder.split(tune):
+        traininglist.append(tune.iloc[train,:])
+        validatelist.append(tune.iloc[validate,:])
+    print("K-fold success")
+    return validatelist, traininglist
+
+###     USING FUNCTIONS
 checkna(data)
 data=drop_duplicates(data)
-
-def split_data(data):
-    train, test = skm.train_test_split(data, test_size=0.15)
-    print("data splitted")
-    print(f"train: {train.shape}\n test: {test.shape}")
+data=maldiRename(data)
+data=createDummies(data)
+tune, test = splitData(data)
+validatelist, traininglist = kfolderino(data)
 
 
